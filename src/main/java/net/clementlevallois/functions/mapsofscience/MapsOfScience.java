47d89ca6906551edfe79b38aa6849a256a89f47c
seinecle <1244100+seinecle@users.noreply.github.com>
2023-04-05 16:00:06 +0200

    first push for the repo

---
+++ src/main/java/net/clementlevallois/functions/mapsofscience/MapsOfScience.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.clementlevallois.functions.mapsofscience;
+
+/**
+ *
+ * @author LEVALLOIS
+ */
+public class MapsOfScience {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
