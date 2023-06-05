9819579abca0990951e180f92ae28b5d43465904
seinecle <1244100+seinecle@users.noreply.github.com>
2023-06-05 23:03:49 +0200

    added more comments in the Controller class

--- src/main/java/net/clementlevallois/functions/mapsofscience/Controller.java
+++ src/main/java/net/clementlevallois/functions/mapsofscience/Controller.java
@@ -28,10 +28,16 @@ public class Controller {
         boolean testOnSmallSample = false;
 
         
+        // THESE STEPS CAN RUN ON A COMPUTER WITH 4Gb or RAM and 100Gb of free disk space.
+        
+        // PREFERABLY A SERVER AS DATA COLLECTION TAKES 20 hours, BUT A LAPTOP WOULD WORK AS WELL
+        
+        // ++++ CAREFUL, THE LAST STEP DESCRIBED BELOW NEEDS A COMPUTER WITH 12GB OF RAM. +++
+        
         /**
-         * The works we received from OpenAlex are json objects.
-         * We will stored them in a txt file as an array of json objects.
-         * So, we need first to create this empty txt file with an opening '[' characters.
+         * The works we are going to receive from OpenAlex are json objects.
+         * We will store them in a txt file as an array of json objects.
+         * So, we need first to create this empty txt file with an opening '[' character, which is the opening char for an array
          */
         
         OperationsOnJson jsonOps = new OperationsOnJson();
@@ -45,9 +51,10 @@ public class Controller {
         apiCalls.getAllWorksPagedWithCursor(worksFileString);
 
         /**
-         * Adds a couple of characters to the txt file to close the json array.
+         * Deletes the final comma and adds a closing ']' to the txt file to close the json array.
          */
         jsonOps.addClosingCharsToJsonArray(worksFileString, worksWellFormatted);
+
         /**
          * Filters the json file to retain only what we need: journal ids and
          * author ids. Takes approx 10 minutes for 20 million works, which is a
@@ -55,17 +62,46 @@ public class Controller {
          */
         JsonStreamingParser parser = new JsonStreamingParser();
         parser.parseJournalIdsAndAuthorIds(worksWellFormatted, journalsAndAuthorsPerWork);
+
         /**
          * Reorganizes journal ids and author ids to end up with: for each line,
          * a journal id followed by all author ids who have published in it.
          * Takes 3 to 10 minutes and needs 4GB of max heap size (add a -Xmx4g
          * paramater in the command line launching the java program). Produces a
          * file close to 1Gb in size.
+         * 
+         * Also, this step now also produces two files:
+         * - one file where each journal id is remapped to ids in the form of a sequence of 0, 1, 2...
+         * - one file where each author id is remapped to ids in the form of a sequence of 0, 1, 2...
+         * 
+         * This is necessary because in the next step, we will use the method suggested by reddit user Ivory2Match to do pairwise comparisons of journals
+         * This method involves manipulating journal ids and author ids in  single big array of integers. For this reason, we turn original ids, which are longs, into ints.
+         * 
          */
         CreateListOfJournalsWithTheirAuthors createList = new CreateListOfJournalsWithTheirAuthors();
         createList.doAllOps(journalsAndAuthorsPerWork, journalsAndAuthors, journalIdFileString, authorIdFileString);
 
-          
+        /*
+
+        THIS LAST STEP NEEDS A LAPTOP OR SERVER WITH 12GB OF RAM BECAUSE ALL THE DATA IS LOADED IN MEMORY
+        
+        The last step is comparing each journal to each other and connect them if they have at least one author in common.
+        
+        This step is performed using the files generated so far.
+        
+                
+        The initial approach used is in JournalSimilaritiesComputer.java
+        I need to test it again, last time it gave me an out of memory error but I think it was just due to my error in setting the max heap size too low.
+        
+        SINCE THEN I have received a suggestion by reddit user Ivory2Much for an alternative approach, which is implemented in PairwiseComparisonsWithArrayOfIntegers.java
+        The approach: https://www.reddit.com/r/java/comments/13rlb26/speeding_up_pairwise_comparisons_to_28_millionsec/jlm0me1/
+        
+        Please refer to this file "PairwiseComparisonsWithArrayOfIntegers.java" where I have added extensive explanations on this approach,
+        which is both very simple (one single array of integers!) and pretty smart in its logic.
+
+        I have not tested it yet.
+        
+        */
           
     }
 }
